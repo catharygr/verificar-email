041d369dd2d4c4a7f7d893cfabcfb9f4e7d8a6ce
cathy <cathy@bubulazi.com>
2022-04-20 14:14:12 +0100

    Diseño de la pagina terminada

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("hola mundo")
+// fetch("https://api.eva.pingutil.com/email?email=test@mail7.io",)
+// .then(response => response.text())
+// .then(result => console.log(result))
+// .catch(error => console.log('error', error)) 
