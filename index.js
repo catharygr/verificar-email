51f69b9909ffd8749b7748f0d83951c4a2aa50a7
cathy <cathy@bubulazi.com>
2022-04-21 12:18:53 +0100

    Recfactorar la funcion usando asiync

--- index.js
+++ index.js
@@ -5,23 +5,24 @@ let spamMensaje = ''
 let entregarMensaje = ''
 btnVerificar.addEventListener("click", verificar)
 
-function verificar() {
-    fetch(`https://api.eva.pingutil.com/email?email=${txtArea.value}`)
-    .then(res => {
-        if( res.ok){
-            mensaje.innerHTML = 'Ha ocurrido un error, inténtelo más tarde'
-            throw new Error('Ha ocurrido un error')
-        } return res.json()
-    })
-    .then(data => {
 
+
+async function verificar() {
+    try {
+        const respuesta = await  fetch(`https://api.eva.pingutil.com/email?email=${txtArea.value}`)
+        if (!respuesta.ok) {
+            mensaje.innerHTML = 'Ha habido un error, inténtalo más tarde'
+            throw new Error('Ha habido un error' )
+        } 
+        const data = await respuesta.json()
         data.data.spam ? spamMensaje = 'ES' : spamMensaje = 'NO ES'
         data.data.deliverable ? entregarMensaje = "SE PUEDE ENTREGAR" : entregarMensaje = "NO SE PUEDE ENTREGAR"
 
         mensaje.innerHTML = `
         El correo electrónico <strong>${data.data.email_address}</strong> proviene de <strong>${data.data.domain}</strong>,</br> <strong>${entregarMensaje}</strong> y <strong>${spamMensaje}</strong> un spam
         `
-    })
 
-    .catch(error => console.error(error))
+    } catch (fallo) {
+        console.error(fallo)
+    }
 }
